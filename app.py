1e860c57bae5e083e047ea72f90073360e50609f
pst <taekgutv@gmail.com>
2021-04-22 17:01:36 +0900

    1,2,3장 hello world ping pong debug mode test

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+#decorator
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+#decorator
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
+
+#decorator
+@app.route("/debug", methods=['GET'])
+def debug_test():
+    return "debug ok";
 
 if __name__ == '__main__':
     app.run()
